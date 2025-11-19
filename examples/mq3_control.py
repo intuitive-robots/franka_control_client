53e2dc682ba590f8852d2c45df106c5d7eccf3f0
Xinkai Jiang <jiangxinkai98@gmail.com>
2025-11-19 17:02:35 +0100

    update example

--- examples/mq3_control.py
+++ examples/mq3_control.py
@@ -16,22 +16,17 @@ if __name__ == "__main__":
     net_manager = init_xr_node_manager("192.168.0.134")
     net_manager.start_discover_node_loop()
     mq3 = MetaQuest3("IRL-MQ3-1")  # You can change the name by using simpubweb
-    # robot = RemoteFranka("192.168.0.52", 5555)
-    # gripper = RemoteFrankaGripper("192.168.0.52", 5557)
-    robot = RemoteFranka("127.0.0.1", 5555)
-    gripper = RemoteFrankaGripper("127.0.0.1", 5557)
+    robot = RemoteFranka("192.168.0.52", 5555)
+    gripper = RemoteFrankaGripper("192.168.0.52", 5557)
     robot.connect()
     gripper.connect()
     gripper.start_gripper_control()
     robot.set_franka_arm_control_mode(ControlMode.CARTESIAN_VELOCITY)
     try:
         while True:
-            # if mq3.device_info is not None:
-            #     print(f"Device Info: {mq3.device_info}")
             input_data = mq3.get_controller_data()
             if input_data is None:
                 continue
-                # print(f"Input Data: {input_data}")
             if input_data["right"]["hand_trigger"]:
                 vel = np.array(input_data["right"]["vel"])
                 angular_vel = np.array(input_data["right"]["ang_vel"])
